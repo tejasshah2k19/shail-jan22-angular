7611c7eb3814cc0696ca23c36fe86bc9d785136c
Tejas <tejas14all@gmail.com>
2022-02-07 16:52:31 +0530

    custom validation

--- src/app/components/reactive-signup/reactive-signup.component.ts
+++ src/app/components/reactive-signup/reactive-signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-signup',
@@ -11,10 +11,10 @@ export class ReactiveSignupComponent implements OnInit {
   constructor() {
 
     this.myForm = new FormGroup({
-      firstName: new FormControl("",[Validators.required,Validators.minLength(3)]),
-      email: new FormControl("",Validators.required),
-      password: new FormControl("",Validators.required),
-      confirmPassword: new FormControl("",Validators.required)
+      firstName: new FormControl("", [Validators.required, Validators.minLength(3)]),
+      email: new FormControl("", Validators.required),
+      password: new FormControl("", [Validators.required, this.passwordStrength]),
+      confirmPassword: new FormControl("", Validators.required)
     })
 
   }
@@ -22,12 +22,58 @@ export class ReactiveSignupComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  saveUser(){
-    console.log(this.myForm.get("firstName")?.errors);
+  passwordStrength(control: AbstractControl): ValidationErrors | null {
+
+    let pwd = control.value as string
+    let cpwd = control.parent?.get("confirmPassword")?.value
+    
+
+    let upperAlphaCount = 0, lowerAlphaCount = 0, digitCount = 0,ln=0;
+    if (pwd.trim().length > 7) {
+      ln = 1 
+      for (let i = 0; i < pwd.length; i++) {
+        if (pwd.charAt(i) >= 'A' && pwd.charAt(i) <= 'Z') {
+          upperAlphaCount = 1
+        }
+        if (pwd.charAt(i) >= 'a' && pwd.charAt(i) <= 'z') {
+          lowerAlphaCount = 1
+        }
+        if (pwd.charAt(i) >= '0' && pwd.charAt(i) <= '9') {
+          digitCount = 1
+        }
+
+      }
+    } 
+    console.log(upperAlphaCount+lowerAlphaCount+digitCount);
+    
+
+    if ((upperAlphaCount+lowerAlphaCount+digitCount) == 3) {
+      return null
+    } else {
+      return {
+        passwordStrength: {
+            "lower":lowerAlphaCount,
+            "upper":upperAlphaCount,
+            "digit":digitCount,
+            "ln":ln
+        }
+      }
+    }
+
+
+
+  }
+
+
+
+  saveUser() {
+    console.log(this.myForm.get("password")?.errors) 
+    console.log(this.myForm.get("password")?.hasError('passwordStrength'));
+    console.log(this.myForm.get("password")?.getError("passwordStrength").lower);
     
     console.log(this.myForm.value);
     console.log(this.myForm.valid);
-    
+
   }
 
 }
